fae1ae60a130487544401558ccad8d6e85bb9220
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2023-09-16 11:53:31 +0530

    still incomplete project

--- BOOKSTORE-MERN/backend/config.js
+++ BOOKSTORE-MERN/backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://xdcoder:aditya@bookstore-mern.r8vccau.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const mongoDBURL = 'mongodb+srv://xdcoder:aditya@bookstore-mern.r8vccau.mongodb.net/?retryWrites=true&w=majority'
