98a0de3033c0fcb9885377f533c7f0aebb786de6
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2023-09-20 21:23:45 +0530

    still unfinished

---
+++ BOOKSTORE-MERN/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBooks'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/ShowBook'
+import DeleteBook from './pages/DeleteBook'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' elements={<Home />}/>
+      <Route path='/books/create' elements={<CreateBook />}/>
+      <Route path='/books/details/:id' elements={<ShowBook />}/>
+      <Route path='/books/edit/:id' elements={<EditBook />}/>
+      <Route path='/books/delete/:id '  elements={<DeleteBook />}/>
+    </Routes>
+  )
+}
+
+export default App
